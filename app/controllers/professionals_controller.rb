d1c187ea01d5e21ddc025921aa1c566d6eeb47e3
arjis02 <rj.ramos@gmail.com>
2015-10-09 16:03:08 -0400

    Update create method to update params for physicians

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,2 +1,39 @@
 class ProfessionalsController < ApplicationController
+	# before_action :updated_params
+
+	def new
+		@professional = Professional.new
+	end
+
+	def create
+		if params[:physician] == '1'
+			updated_params = professional_params.merge(physician: true)
+		else
+			updated_params = professional_params.merge(physician: false)
+		end
+
+		professional = Professional.new(updated_params)
+
+		if professional.save
+			redirect_to '/'
+		else
+			flash[:error] = professional.errors.full_messages
+			redirect_to new_professional_path
+		end
+	end
+
+	private
+
+	def professional_params
+		params.require(:professional).permit(:first_name, :last_name, :email, :specialty, :registered_state, :phone_number, :license_number, :physician, :password)
+	end
+
+	# def updated_params
+	# 	if params[:physician] == "1"
+	# 		professional_params.merge(physician: true)
+	# 	else
+	# 		professional_params
+	# 	end
+	# end
+
 end
