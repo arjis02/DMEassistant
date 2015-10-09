33a32387b328ef20e524a7226fb5476f457b39c5
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-09 13:16:59 -0400

    Add gender to factory for patients

--- spec/factories/patient.rb
+++ spec/factories/patient.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
 	factory :patient do
-		first_name { Faker::Name.name }
-		last_name { Faker::Name.name }
+		first_name { Faker::Name.first_name }
+		last_name { Faker::Name.last_name }
 		email { Faker::Internet.email }
 		street_address { Faker::Address.street_address }
 		city { Faker::Address.city }
@@ -11,6 +11,7 @@ FactoryGirl.define do
 		height { Faker::Number.number(2) }
 		weight { Faker::Number.number(3) }
 		birthday { Faker::Date.between(10000.days.ago, 15000.days.ago) }
+		gender { ["M", "F"].sample }
 		discharge_date { Faker::Date.forward(5) }
 	end
 end
