f702faec235d5f100229a967936aee2cda7390fb
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-09 10:23:43 -0400

    Add gender column to patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -19,7 +19,7 @@ class PatientsController < ApplicationController
 	private
 
 	def patient_params
-		params.require(:patient).permit(:first_name, :last_name, :email, :street_address, :city, :state, :zipcode, :phone_number, :height, :weight, :notes, :discharge_date)
+		params.require(:patient).permit(:first_name, :last_name, :email, :street_address, :city, :state, :zipcode, :phone_number, :height, :weight, :notes, :gender, :birthday, :discharge_date)
 	end
 
 end
--- db/migrate/20151008002632_patients.rb
+++ db/migrate/20151008002632_patients.rb
@@ -12,6 +12,7 @@ class Patients < ActiveRecord::Migration
   		t.string   :notes
       t.string   :height, null: false, limit: 128
       t.string   :weight, null: false, limit: 128
+      t.string   :gender, limit: 8
       t.date     :birthday
       t.date     :discharge_date
 
