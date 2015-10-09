1217c1cbde614022db86da7ee909890589bb8fcf
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-09 01:16:48 -0400

    Create Patients controller

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,25 @@
+class PatientsController < ApplicationController
+
+	def new
+		@patient = Patient.new
+	end
+
+	def create
+		patient = Patient.new(patient_params)
+
+		if patient.save
+			redirect_to dashboard_therapists_path if session[:therapist_id]
+			redirect_to dashboard_physicians_path if session[:physician_id]
+		else
+			flash[:error] = patient.errors.full_messages
+			redirect_to new_patient_path
+		end
+	end
+
+	private
+
+	def patient_params
+		params.require(:patient).permit(:first_name, :last_name, :email, :street_address, :city, :state, :zipcode, :phone_number, :height, :weight, :notes, :discharge_date)
+	end
+
+end
