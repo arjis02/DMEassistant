4c1d93f56b0383cac5c8277e8b2e0a314e5ba657
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-12 16:19:11 -0400

    Modify seed to assign patients and facilities through admissions table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+Order.delete_all
+Admission.delete_all
 Patient.delete_all
 Facility.delete_all
 
@@ -5,6 +7,6 @@ Facility.delete_all
 	facility = FactoryGirl.create(:facility)
 	10.times do
 		patient = FactoryGirl.create(:patient)
-		facility.patients << patient
+		admission = Admission.create(patient_id: patient.id, facility_id: facility.id)
 	end
 end
