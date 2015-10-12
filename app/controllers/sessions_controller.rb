f99add67bd5b8c530453bd6e1f0158456eac05ef
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-11 20:18:05 -0400

    Refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,11 @@
 class SessionsController < ApplicationController
 
 	def create
-		physician = Physician.find_by(email: params[:session][:email])
-		therapist = Therapist.find_by(email: params[:session][:email])
-		dme = Dme.find_by(email: params[:session][:email])
+		professional = Professional.find_by(email: params[:session][:email])
 		
-		if physician && physician.authenticate(params[:session][:password])
-			session[:physician_id] = physician.id
-			redirect_to dashboard_physicians_path
-		elsif therapist && therapist.authenticate(params[:session][:password])
-			session[:therapist_id] = therapist.id
-			redirect_to dashboard_therapists_path
-		elsif dme && dme.authenticate(params[:session][:password])
-			session[:dme_id] = dme.id 
-			redirect_to dashboard_dmes_path
+		if professional && professional.authenticate(params[:session][:password])
+			session[:professional_id] = professional.id
+			redirect_to '/dashboard'
 		else
 			flash[:error] = ["Unsuccessful Login"]
 			redirect_to '/'
