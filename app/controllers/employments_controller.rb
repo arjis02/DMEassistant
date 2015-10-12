8c699ee74ab184041e50f1ca01e0dd075d64329b
arjis02 <rj.ramos@gmail.com>
2015-10-12 11:25:13 -0400

    Create employments controller file

---
+++ app/controllers/employments_controller.rb
@@ -0,0 +1,17 @@
+class EmploymentsController < ApplicationController
+	def create
+		professional = Professional.find(session[:professional_id])
+		facility = Facility.find(params[:employments][:facility_id])
+
+		relationship = Employment.new(professional_id: professional.id, facility_id: facility.id)
+		if relationship.save
+			redirect_to dashboard_path
+		else
+			flash[:error] = relationship.errors.full_messages
+			redirect_to facility_path(facility)
+		end
+	end
+
+	def destroy
+	end
+end
