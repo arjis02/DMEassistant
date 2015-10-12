e45d0b5614fef20485f08919077af4e7b7ea81df
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-11 20:22:46 -0400

    Refactor redirects to dashboard_path instead of /dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
 	def index
-		redirect_to '/dashboard' if session[:professional_id]
+		redirect_to dashboard_path if session[:professional_id]
 	end
 end
