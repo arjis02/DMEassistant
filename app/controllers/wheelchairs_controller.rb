345d194f90b6605cc9aa69dc43fe045f964058f9
Ramon Ramos III <rj.ramos@gmail.com>
2015-10-28 17:47:49 -0400

    Create new and create paths

--- app/controllers/wheelchairs_controller.rb
+++ app/controllers/wheelchairs_controller.rb
@@ -1,3 +1,22 @@
 class WheelchairsController < ApplicationController
-	belongs_to :order
+	def new
+		@wheelchair = Wheelchair.new()
+	end
+
+	def create
+		@wheelchair = Wheelchair.new(wheelchair_params)
+
+		if @wheelchair.save
+			render "order/show"
+		else
+			flash[:error] = wheelchair.errors.full_messages
+			redirect_to new_order_path
+		end
+	end
+
+	private
+
+	def wheelchair_params
+		params.require(:wheelchair).permit(:type, :front_riggings, :arms, :order_id)
+	end
 end
