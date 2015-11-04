84546a04bd9009d015facc736ef2f09766710acd
arjis02 <rj.ramos@gmail.com>
2015-11-04 10:14:14 -0500

    Add ActiveRecord::Enum for Wheelchair options

--- app/models/wheelchair.rb
+++ app/models/wheelchair.rb
@@ -1,3 +1,7 @@
 class Wheelchair < ActiveRecord::Base
 	belongs_to :order
+
+	enum wc_type:        [ :standard, :lightweight, :hd, :ex_hd ]
+	enum front_riggings: [ :std_fr, :elev_lr, :hd_lr, :art_lr ]
+	enum arms:           [ :desk, :full ]
 end
